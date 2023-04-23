17a271a4fc2d1f82f1fe6acea79b75647b864448
LAPTOP-G9CBJI3P\tantrungse <tantrungse@gmail.com>
2023-04-23 15:28:09 +0700

    Player Controls and Animations

---
+++ Assets/Scripts/PlayerMoveControls.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveControls : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+
+    private GatherInput gI;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private int direction = 1;
+    private bool doubleJump = true;
+    public int additionalJumps = 2;
+    private int resetJumpsNumber;
+
+    public float rayLength;
+    public LayerMask groundLayer;
+    public Transform leftPoint;
+    public Transform rightPoint;
+    private bool grounded = true;
+
+    void Start()
+    {
+        gI = GetComponent<GatherInput>();
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        resetJumpsNumber = additionalJumps;
+    }
+
+    void Update()
+    {
+        SetAnimatorValues();
+    }
+
+    private void FixedUpdate()
+    {
+        CheckStatus();
+        Move();
+        JumpPlayer();
+    }
+
+    private void Move()
+    {
+        Flip();
+        rb.velocity = new Vector2(speed * gI.valueX, rb.velocity.y);
+    }
+
+    public void JumpPlayer()
+    {
+        if (gI.jumpInput)
+        {
+            if (grounded)
+            {
+                rb.velocity = new Vector2(gI.valueX * speed, jumpForce);
+                doubleJump = true;
+            } else if (additionalJumps > 0)
+            {
+                rb.velocity = new Vector2(gI.valueX * speed, jumpForce);
+                doubleJump = false;
+                additionalJumps -= 1;
+            }
+        }
+        gI.jumpInput = false;
+    }
+
+    private void CheckStatus()
+    {
+        RaycastHit2D leftCheckHit = Physics2D.Raycast(leftPoint.position, Vector2.down, rayLength, groundLayer);
+        RaycastHit2D rightCheckHit = Physics2D.Raycast(rightPoint.position, Vector2.down, rayLength, groundLayer);
+
+        if (leftCheckHit || rightCheckHit)
+        {
+            grounded = true;
+            doubleJump = false;
+            additionalJumps = resetJumpsNumber;
+        } 
+        else
+        {
+            grounded = false;
+        }
+        SeeRays(leftCheckHit, rightCheckHit); 
+    }
+
+    private void SeeRays(RaycastHit2D leftCheckHit, RaycastHit2D rightCheckHit)
+    {
+        Color color1 = leftCheckHit ? Color.red : Color.green;
+        Color color2 = rightCheckHit ? Color.red : Color.green;
+        Debug.DrawRay(leftPoint.position, Vector2.down * rayLength, color1);
+        Debug.DrawRay(rightPoint.position, Vector2.down * rayLength, color2);
+    }
+
+    private void Flip()
+    {
+        if (gI.valueX * direction < 0)
+        {
+            transform.localScale = new Vector3(-transform.localScale.x, 1, 1);
+            direction *= -1;
+        }
+    }
+
+    private void SetAnimatorValues()
+    {
+        anim.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+        anim.SetFloat("vSpeed", rb.velocity.y);
+        anim.SetBool("Grounded", grounded);
+    }
+}
